0310b194f9902aad118c1ead7846fc4578e5f7ec
NikolaCredeo <103927078+NikolaCredeo@users.noreply.github.com>
2022-11-24 16:10:55 +0100

    refactoring header of calendar, updated version

--- lib/src/widgets/calendar_header.dart
+++ lib/src/widgets/calendar_header.dart
@@ -39,59 +39,59 @@ class CalendarHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final text = headerStyle.titleTextFormatter?.call(focusedMonth, locale) ??
-        DateFormat.yMMMM(locale).format(focusedMonth);
+    final text = headerStyle.titleTextFormatter?.call(focusedMonth, locale) ?? DateFormat.yMMMM(locale).format(focusedMonth);
 
-    return Container(
-      decoration: headerStyle.decoration,
-      margin: headerStyle.headerMargin,
-      padding: headerStyle.headerPadding,
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          if (headerStyle.leftChevronVisible)
-            CustomIconButton(
-              icon: headerStyle.leftChevronIcon,
-              onTap: onLeftChevronTap,
-              margin: headerStyle.leftChevronMargin,
-              padding: headerStyle.leftChevronPadding,
-            ),
-          Expanded(
-            child: headerTitleBuilder?.call(context, focusedMonth) ??
-                GestureDetector(
-                  onTap: onHeaderTap,
-                  onLongPress: onHeaderLongPress,
-                  child: Text(
-                    text,
-                    style: headerStyle.titleTextStyle,
-                    textAlign: headerStyle.titleCentered
-                        ? TextAlign.center
-                        : TextAlign.start,
-                  ),
-                ),
-          ),
-          if (headerStyle.formatButtonVisible &&
-              availableCalendarFormats.length > 1)
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: FormatButton(
-                onTap: onFormatButtonTap,
-                availableCalendarFormats: availableCalendarFormats,
-                calendarFormat: calendarFormat,
-                decoration: headerStyle.formatButtonDecoration,
-                padding: headerStyle.formatButtonPadding,
-                textStyle: headerStyle.formatButtonTextStyle,
-                showsNextFormat: headerStyle.formatButtonShowsNext,
+    return LayoutBuilder(
+      builder: (context, constraints) => Container(
+        decoration: headerStyle.decoration,
+        margin: headerStyle.headerMargin,
+        padding: headerStyle.headerPadding,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            // this alignes header text to monday
+            SizedBox(width: constraints.maxWidth / 7 / 2.4),
+            GestureDetector(
+              onTap: onHeaderTap,
+              onLongPress: onHeaderLongPress,
+              child: Text(
+                text,
+                style: headerStyle.titleTextStyle,
+                textAlign: headerStyle.titleCentered ? TextAlign.center : TextAlign.start,
               ),
             ),
-          if (headerStyle.rightChevronVisible)
-            CustomIconButton(
-              icon: headerStyle.rightChevronIcon,
-              onTap: onRightChevronTap,
-              margin: headerStyle.rightChevronMargin,
-              padding: headerStyle.rightChevronPadding,
-            ),
-        ],
+            const Spacer(),
+            if (headerStyle.formatButtonVisible && availableCalendarFormats.length > 1)
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: FormatButton(
+                  onTap: onFormatButtonTap,
+                  availableCalendarFormats: availableCalendarFormats,
+                  calendarFormat: calendarFormat,
+                  decoration: headerStyle.formatButtonDecoration,
+                  padding: headerStyle.formatButtonPadding,
+                  textStyle: headerStyle.formatButtonTextStyle,
+                  showsNextFormat: headerStyle.formatButtonShowsNext,
+                ),
+              ),
+            if (headerStyle.leftChevronVisible)
+              CustomIconButton(
+                icon: headerStyle.leftChevronIcon,
+                onTap: onLeftChevronTap,
+                margin: headerStyle.leftChevronMargin,
+                padding: headerStyle.leftChevronPadding,
+              ),
+            // if (headerStyle.leftChevronVisible && headerStyle.rightChevronVisible)
+            //   const SizedBox(width: 20),
+            if (headerStyle.rightChevronVisible)
+              CustomIconButton(
+                icon: headerStyle.rightChevronIcon,
+                onTap: onRightChevronTap,
+                margin: headerStyle.rightChevronMargin,
+                padding: headerStyle.rightChevronPadding,
+              ),
+          ],
+        ),
       ),
     );
   }
